13c686a1418edd9b017808e778067b04359b5e39
Katherine <katherinerahmed@gmail.com>
2015-12-25 12:37:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+def position_taken?(board, position)
+  if board[0]== " " || nil
+    return false
+  elsif board[0]== ""
+    return false
+  elsif board[0]== nil
+    return false
+  else
+    return true
+  end
+end
